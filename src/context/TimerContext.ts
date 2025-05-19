9e4f0a1782b0cb2f6cef9330cba398ebc80efd97
Amit Efrati <aefrati@beyondtrust.com>
2025-05-19 15:26:06 +0300

    Revert "Initial commit"
    This reverts commit 3282c639f72ce19d1949e444bc630a643bc79c1c.

--- src/context/TimerContext.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import type { TimerContextType } from "./TimerProvider";
-
-export const TimerContext = createContext<TimerContextType | undefined>(undefined);
